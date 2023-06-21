711fec5ab1143dc55a07125d2b73fcd9017818d2
Fandika Ikhsan <fandika.ikhsan@gmail.com>
2023-06-21 07:40:10 +0000

    fix: add type definition for uploading feat

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-import express from 'express';
+import express, { Request } from 'express';
 import multer from 'multer';
+
+type DestinationCallback = (error: Error | null, destination: string) => void
+type FileNameCallback = (error: Error | null, filename: string) => void
+type FileFilterCallback = (error: Error | null, acceptFile: boolean) => void
+
 
 const app = express();
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
+    destination: (req: Request, file: Express.Multer.File, cb: DestinationCallback) => {
         cb(null, 'public/images');
     },
-    filename: (req, file, cb) => {
+    filename: (req: Request, file: Express.Multer.File, cb: FileNameCallback) => {
         const fileName = file.originalname.toLowerCase().split(' ').join('-');
         cb(null, fileName);
     }
@@ -15,7 +20,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    fileFilter: (req, file, cb) => {
+    fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
         if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
             cb(null, true);
         } else {
