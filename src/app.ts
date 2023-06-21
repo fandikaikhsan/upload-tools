e4abd2c141fb8c23b698dd4f3fb18c366fbe746d
Fandika Ikhsan <fandika.ikhsan@gmail.com>
2023-06-21 07:14:17 +0000

    feat: add multer types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("hello")
+import express from 'express';
+import multer from 'multer';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Server is listening on port http://localhost:3000');
+    }
+);
