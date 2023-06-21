2adc9da2375e3414309f98602e18194dd134a0b3
Fandika Ikhsan <fandika.ikhsan@gmail.com>
2023-06-21 07:35:48 +0000

    feat: storage upload route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,38 @@ import multer from 'multer';
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    }
+});
+
+app.post('/upload', upload.single('image'), (req, res, next) => {
+    const file = req.file;
+    if (!file) {
+        const error = new Error('Please upload a file');
+        return next(error);
+    }
+    res.send(file);
+    }
+);
+
 app.get('/', (req, res) => {
     res.send('Hello world!');
     }
