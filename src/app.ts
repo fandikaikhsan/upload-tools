025a75abdfa220cf65d9a94107380684322c9893
Fandika Ikhsan <fandika.ikhsan@gmail.com>
2023-06-21 07:40:44 +0000

    chore: comment out error cb response

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ const upload = multer({
             cb(null, true);
         } else {
             cb(null, false);
-            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+            // return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
         }
     }
 });
